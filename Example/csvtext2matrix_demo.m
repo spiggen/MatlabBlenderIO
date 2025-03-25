8f9bdb86618fded0015d4e187a6ff9569a1b3322
spiggen <jonasvilgotlotberg@hotmail.se>
2025-03-25 19:10:36 +0100

    updated doc

--- Example/csvtext2matrix_demo.m
+++ Example/csvtext2matrix_demo.m
@@ -1,3 +1,12 @@
+%% install MatlabBlenderIO if not done so already, and add to path.
+[library_failiure, library_status] = system("pip show MatlabBlenderIO");
+if library_failiure
+[install_failiure, install_status] = system("pip install MatlabBlenderIO");
+[library_failiure, library_status] = system("pip show MatlabBlenderIO");
+end
+addpath(genpath(strip(extractBetween(string(library_status), "Location: ", "Requires:"))+"\MatlabBlenderIO"));
+
+
 [testmat, dimensions] = csvtext2matrix("0,0,0,,0,0,0,,,0,0,0,,0,0,0,,,0,0,0,,0,0,0")
 [testmat, dimensions] = csvtext2matrix("0,0,0,,0,0,0,,,0,0,0,,0,0,0")
 [testmat, dimensions] = csvtext2matrix("0,0,0,,0,0,0")
---
+++ installer.bat
@@ -0,0 +1,2 @@
+call pip install MatlabBlenderIO -U
+call & "C:\Program Files\Blender Foundation\Blender 3.6\3.6\python\bin\python.exe" -m pip install MatlabBlenderIO -U
